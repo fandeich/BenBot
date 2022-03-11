7130ba0a08b6bce983f7cf788ac5e2d3e9c0b555
fandeich <fandeichdani@gmail.com>
2022-03-11 15:08:42 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"gopkg.in/telegram-bot-api.v4"
+	"math/rand"
+	"time"
+)
+
+func telegramBot() {
+	bot, err := tgbotapi.NewBotAPI("5247616780:AAF4iFBQCMuMLQRwS7gVXSwboPlihXcI3Qg")
+	if err != nil {
+		panic(err)
+	}
+	rand.Seed(time.Now().UnixNano())
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if update.Message.Text != "" {
+			switch update.Message.Text {
+			case "/start":
+				msg := tgbotapi.NewAudioUpload(update.Message.Chat.ID, "voice/himself.ogg")
+				_, err = bot.Send(msg)
+				if err != nil {
+					panic(err)
+				}
+			default:
+				if rand.Intn(100) < 50 {
+					msg := tgbotapi.NewAudioUpload(update.Message.Chat.ID, "voice/noo.ogg")
+					_, err = bot.Send(msg)
+					if err != nil {
+						panic(err)
+					}
+				} else {
+					msg := tgbotapi.NewAudioUpload(update.Message.Chat.ID, "voice/yes.ogg")
+					_, err = bot.Send(msg)
+					if err != nil {
+						panic(err)
+					}
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	telegramBot()
+}
